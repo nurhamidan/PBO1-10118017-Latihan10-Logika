2bca0de7bc60865d3464c6223145e82313829ff1
Agung Nurhamidan <>
2019-09-22 18:09:37 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,26 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Menampilkan status dari operasi logika pada variabel dewasa.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status dewasa\t: " + dewasa);
+    }
+    
+}
